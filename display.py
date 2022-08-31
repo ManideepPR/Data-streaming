f33568db271b90ecd8f9eaaf07d7ca41a7c0da73
Manideep P R <manideep1042@gmail.com>
2022-08-31 14:29:45 +0530

    Add files via upload

---
+++ display.py
@@ -0,0 +1,61 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import json
+from json import dumps
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+
+
+# sc = SparkContext("local[2]", "NetworkWordCount")
+# ssc = StreamingContext(sc, 1)
+# lines = ssc.socketTextStream("localhost", 5556)
+# words = lines.flatMap(lambda line: line.split(" "))
+# pairs = words.map(lambda word: (word, 1))
+# wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+# # Print the first ten elements of each RDD generated in this DStream to the console
+# wordCounts.pprint()
+def send_to_kafka(rows):
+    #producer = KafkaProducer(bootstrap_servers = util.get_broker_metadata())
+        word = rows[0]
+        count = rows[1]
+        print(rows)
+        print(type(word))
+        producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'], api_version=(0, 10, 1), value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+        v= {rows[0]:rows[1]}
+        if(word == 'MI' or word == "RCB" or word == 'SRH' or word == 'GT'):
+            producer.send(word,value=v)
+            print("============================got a tweet==================================");
+            #producer.flush()
+
+
+if __name__ == "__main__":
+    
+    sc = SparkContext("local[2]", "NetworkWordCount")
+    ssc = StreamingContext(sc, 60)
+    lines = ssc.socketTextStream("localhost", 5556)
+    words = lines.flatMap(lambda line: line.split(" "))
+    pairs = words.map(lambda word: (word, 1))
+    wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+    wordCounts.foreachRDD(lambda rdd: rdd.foreach(send_to_kafka))
+    ssc.start()             
+    ssc.awaitTermination()
+    
+
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+
+def on_send_error(excp):
+    pass
+
---
+++ get_tweets.py
@@ -0,0 +1,57 @@
+
+import tweepy
+from tweepy.auth import OAuthHandler
+from tweepy import Stream
+import socket
+import json
+import os
+
+class TweetsListener(tweepy.Stream):
+
+    def __init__(self, csocket, *args):
+        super().__init__(*args)
+        self.client_socket = csocket
+
+    def on_data(self, data):
+        try:
+            message = json.loads(data)
+            print(message['text'].encode('utf-8'))
+            self.client_socket.send(message['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def if_error(self, status):
+        print(status)
+        return True
+
+
+def send_tweets(c_socket):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    twitter_stream = TweetsListener(
+        c_socket, consumer_key, consumer_secret, access_token, access_secret)
+    
+    twitter_stream.filter(track=['ipl'])
+
+
+if __name__ == "__main__":
+    consumer_key = "kfLBGa8SzFFVXb8yDD05ujFBK"
+    consumer_secret = "eeRF19jxEastmCtp0CYj4X86ZKfiOCNb6KzAnXGmJdrQN7q2vO"
+    access_token = "942255638205177858-KiRafYc6uQDkCmXUIracz3ljk6KT6NM"
+    access_secret = "WLzqah0oGNN46KpYlWZpcETU7NUPJI9ng3SvFVkqhfV12"
+    new_skt = socket.socket()         
+    host = "127.0.0.1"     
+    port = 5556              
+    new_skt.bind((host, port))       
+
+    print("port: %s" % str(port))
+
+    new_skt.listen(5) 
+  
+    c, addr = new_skt.accept()
+
+    print("got request: " + str(addr))
+    send_tweets(c)
